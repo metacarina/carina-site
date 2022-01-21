cfa60202ec3852a5b7442f56006d236f0e153ef5
Unknown <metacarina@gmail.com>
2022-01-21 17:48:22 -0500

    add cognito auth

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_t9RV4ciZL', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1md1on3au4racisa522e7ohj0k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
